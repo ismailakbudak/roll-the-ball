638db687e8e0ac0b003b937c9a65e0ea9f46aa51
İsmail Akbudak <ismail.akbudak@lab2023.com>
2016-12-24 22:09:55 +0200

    Make pick up objects as collectable

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,18 +7,21 @@ public class PlayerController : MonoBehaviour {
 
 	private Rigidbody rb;
 
-	void Start ()
-	{
+	void Start (){
 		rb = GetComponent<Rigidbody>();
 	}
 
-	void FixedUpdate ()
-	{
+	void FixedUpdate (){
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Pick Up"))
+			other.gameObject.SetActive(false);
 	}
 }
